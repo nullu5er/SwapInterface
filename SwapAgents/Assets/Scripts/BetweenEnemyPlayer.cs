50bdf95f15d25f0b018bb17a9b62afbffad011fa
Jeff <jeffsaieinfo@gmail.com>
2022-02-23 09:43:23 -0800

    Added a few simple behavior classes unimplemented

---
+++ SwapAgents/Assets/Scripts/BetweenEnemyPlayer.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BetweenEnemyPlayer : BehaviorBase
+{
+    public override Vector3 GetForce(float maxForce)
+    {
+        throw new System.NotImplementedException();
+    }
+}
---
+++ SwapAgents/Assets/Scripts/CloseCombat.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloseCombat : BehaviorBase
+{
+    public override Vector3 GetForce(float maxForce)
+    {
+        throw new System.NotImplementedException();
+    }
+
+}
